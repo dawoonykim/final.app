c8594f12ac5126e5e54ae98fbc55a6391a95257c
dawoonramykim <134495056+dawoonramykim@users.noreply.github.com>
2023-05-28 22:34:44 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ st.title("Clustering - K-medoids / K-Means")
 st.title("")
 st.header("**Clustering - 군집분석**")
 st.markdown("")
-st.markdown("'Clustering(군집분석)' :red[비지도학습](unsupervised learning)의 일종으로 기준이 없는 상태에서 주어진 데이터의 속성값들을 고려해 :blue[유사한 데이터끼리 그룹화를 시키는 학습 모델]을 말한다. 각 데이터의 유사성을 측정하여, 유사성이 높은 집단끼리  분류하고 군집간에 상이성을 규명하는 방법이다.")
+st.markdown("'Clustering(군집분석)'은 :red[비지도학습](unsupervised learning)의 일종으로 기준이 없는 상태에서 주어진 데이터의 속성값들을 고려해 :blue[유사한 데이터끼리 그룹화를 시키는 학습 모델]을 말한다. 각 데이터의 유사성을 측정하여, 유사성이 높은 집단끼리  분류하고 군집간에 상이성을 규명하는 방법이다.")
 st.markdown("")
 img = Image.open("clustering.jpg")
 st.image(
